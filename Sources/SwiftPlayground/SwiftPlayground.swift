f4102efaac8ece199a9dc96fb571dac4ea58a569
oc-caleb-samuel-25 <caleb.samuel@student.onslow.school.nz>
2026-02-10 10:25:06 +0000

    var and constants interpolation

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,15 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let school = "Onslow College"
+        let hours = "6"
+
+        var name = "sam"  // String values need quotes
+        var age = 12
+        var yearLevel = 13
+
+       let message = "Hi I am \(name), I am a \(age) year old student attending \(school) and im currently in year \(yearLevel). Today school lasted for \(hours) hours."
+
+       print(message)
     }
 }
