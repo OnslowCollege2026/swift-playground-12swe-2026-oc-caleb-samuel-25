327aebccc65edb0e62b4bed2e014d8237e64c85f
oc-caleb-samuel-25 <caleb.samuel@student.onslow.school.nz>
2026-02-23 21:59:23 +0000

    Feb 23, 2026, 9:59 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,32 +1,6 @@
-func roomArea(length: Double, width: Double) -> Double {
-    return length * width
-}
-
-func roomHeight() -> Double {
-    return 2.7
-}
-
-func roomArea() -> Double {
-    return 6.0 * 4.5
-}
-
-let roomLength = 6.0
-let roomWidth = 4.5
-
-func roomArea() -> Double {
-    return roomLength * roomWidth
-}
-
-func isLargeRoom() -> Bool {
-    return true
-}
-
-
-
-
 @main
 struct SwiftPlayground {
     static func main() {
-
+}
     }
 }
