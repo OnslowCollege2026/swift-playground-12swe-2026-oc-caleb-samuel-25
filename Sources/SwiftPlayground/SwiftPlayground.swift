1eb468a26e91cb19a8844dec755628120977b40e
oc-caleb-samuel-25 <caleb.samuel@student.onslow.school.nz>
2026-03-04 21:26:13 +0000

    Mar 4, 2026, 9:26 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,11 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        let temp = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
 
+        ]
     }
 }
