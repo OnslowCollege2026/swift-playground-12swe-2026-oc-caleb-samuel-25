8b2998a4758c79c0b3374e211e1b98bde1ba0acb
oc-caleb-samuel-25 <caleb.samuel@student.onslow.school.nz>
2026-03-31 05:07:28 +0000

    Commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,15 +1,64 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let school = "Onslow College"
-        let hours = "6"
+        let vocabulary: [[String]] = [
+            ["hello", "yodie", "chur", "yoza", "anuck"],
+            ["goodbye", "cya","laters", "peace", "cnucks yo"],
+            ["water", "wata", "watux", "watickle", "wanuk"]
+        ]
+    
+    var counter = 0
+    var score = 0
+    var incorrectanswerIndice : [Int] = []
 
-        var name = "sam"  // String values need quotes
-        var age = 12
-        var yearLevel = 13
+    while counter < vocabulary.count {
+        let englishWord = vocabulary[counter][0]
+        let correctWord = vocabulary[counter][1]
+        let allAnswers = vocabulary[counter].dropFirst().shuffled()
 
-       let message = "Hi I am \(name), I am a \(age) year old student attending \(school) and im currently in year \(yearLevel). Today school lasted for \(hours) hours."
 
-       print(message)
+        print("Please translate \(englishWord)")
+        allAnswers.forEach { answer in
+        print("-\(answer)")
+
+        }
+        
+        if let userInput = readLine(), userInput.lowercased() == correctWord.lowercased() {
+            score = score + 1
+            print("Yes, \(correctWord) is correct")
+        } else {
+            incorrectanswerIndice.append(counter)
+            print("sorry! the correct answer is \(correctWord)")
+        }
+        counter = counter + 1
+    }
+    while incorrectanswerIndice.count > 0 {
+        let index = incorrectanswerIndice[0]
+        let englishWord = vocabulary[index][0]
+        let correctWord = vocabulary[index][1]
+        let allAnswers = vocabulary[index].dropFirst().shuffled()
+
+
+        print("Please translate \(englishWord)")
+        allAnswers.forEach { answer in
+        print("-\(answer)")
+
+if let userInput = readLine(), userInput.lowercased() == correctWord.lowercased() {
+            incorrectanswerIndice.removeFirst()
+            print("Yes, \(correctWord) is correct")
+        } else {
+            incorrectanswerIndice.append(counter)
+            print("sorry! the correct answer is \(correctWord)")
+        }
+    }
+
+        }
+    print("You have a score of \(score)/\(vocabulary.count)")
+    if Double(score) >= Double(vocabulary.count / 2) {
+        print("Congratulations!")
+    } else {
+        print("Try again next time")
+    }
     }
 }
+
