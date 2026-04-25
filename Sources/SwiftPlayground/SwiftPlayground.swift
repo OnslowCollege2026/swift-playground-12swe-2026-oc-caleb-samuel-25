4b15d120e89271eac037b995b5d0c05441f90e01
oc-caleb-samuel-25 <caleb.samuel@student.onslow.school.nz>
2026-04-25 04:26:46 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,43 +1,83 @@
-
 /// This function is going to be used to show the user all the options they have to choose from the program.
 func menuChoice() -> Int {
     print("==== Egg Shop ====")
     print("1. Add Kumara")
-    print("2. Sell Kumara")
+    print("2. Record a sale")
     print("3. Show current stock")
     print("4. Show total Kumara sold")
-    print("5. Exit")
+    print("5. Owner summary")
+    print("6. Exit")
     print("Choose an option:")
     let input = readLine() ?? ""
     let number = Int(input)
     return number ?? 0
 }
 
-/// This function is keeping track of the kumara that is being added to the store as an option. 
-func addKumara(currentStock: Int, amount: Int) -> Int {
+func addStock(currentStock: Double, amount: Double) -> Double? {
+    if amount <= 0 {
+        return nil
+    }
+    if currentStock + amount > 50 {
+        return nil
+    }
     return currentStock + amount
 }
 
-/// This function is being used to sell kumara as an option.
-func sellKumara(currentStock: Int, amount: Int) -> Int? {
-    if amount <= currentStock {
-        return currentStock - amount
-    } else {
-        return nil
-    }
-}
-/// This function is being used to keep track of the total amount of kumara that is being sold.
-func updateSoldCount(currentSold: Int, amount: Int) -> Int {
-    return  currentSold + amount
+func recordSale(currentStock: Double, weight: Double, bags: Int) -> Bool {
+    if bags <= 0 { return false }
+    if weight < 0.1 { return false }
+    if weight > currentStock { return false }
+    if weight > Double(bags) { return false }
+    return true
 }
 
+func calculateCharge(weight: Double, bags: Int) -> String {
+    let kumaraCharge = weight * 3.0
+    let bagCharge = Double(bags) * 0.20
+    let total = kumaraCharge + bagCharge
+    return "Kumara: $\(kumaraCharge), Bags $\(bagCharge), total $\(total))"
+}
+
+func stockMessages(stock: Double) -> String {
+    return "Current Stock: (stock:))kg"
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        var kumaraInStock = 0
-        var kumaraSold = 0
+        var stock: Double = 0.0
+        var sales: [[Double]] = []
 
-        
+        var choice = 0
+
+        while choice != 6 {
+            choice = menuChoice()
+        }
+
+        if choice == 1 {
+            print("How many kgs would you like to add?")
+            let input = readLine() ?? ""
+            let amount = Double(input) ?? 0
+            if let newStock = addStock(currentStock: stock, amount: amount) {
+                stock = newStock
+                print("Added! \(stockMessages(stock: stock))")
+            } else {
+                print("Invalid amount, please try again!")
+            }
+        }
+
+    } else if choice == 2 {
+        print("Enter weight sold \(kg):")
+        let weightInput = readLine () ?? ""
+        print("Enter number of bags used")
+        let bagInput = readLine() ?? ""
+        let weightInput = Double(weightInput) ?? 0
+        let bags = Int(bagInput) ?? 0
+        if recordSale(currentStock: stock, weight: weight, bags: bags) {
+            sales.append([weight, Double(bags)])
+            stock -= weight
+            print("Sale recorded")
+        }
     }
 }
+
